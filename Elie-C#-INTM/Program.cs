792cd869a9f7ed258340553ff7a6926df5bcdb5c
William <william.jaeger57@gmail.com>
2024-09-06 17:19:29 +0200

    avec squelette

--- Elie-C#-INTM/Program.cs
+++ Elie-C#-INTM/Program.cs
@@ -11,5 +11,24 @@ namespace Elie_C__INTM
         static void Main(string[] args)
         {
         }
+
+        public static class ElementaryOperations
+        {
+            public static void BasicOperation(int a = 3, int b = 4, char c = '+')
+            {
+                //TODO
+            }
+
+            public static void IntegerDivision(int a, int b)
+            {
+                //TODO
+            }
+
+            public static void Pow(int a, int b)
+            {
+                //TODO
+            }
+        }
     }
+
 }
