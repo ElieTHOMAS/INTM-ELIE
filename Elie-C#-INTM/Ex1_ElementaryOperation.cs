f1145d7d2cc172e3f255159f08d4ae04fcd16d79
William <william.jaeger57@gmail.com>
2024-09-09 14:17:09 +0200

    exercice 1 fini

--- Elie-C#-INTM/Ex1_ElementaryOperation.cs
+++ Elie-C#-INTM/Ex1_ElementaryOperation.cs
@@ -10,23 +10,23 @@ using System.Threading.Tasks;
 namespace Serie_I
 {
     public static class ElementaryOperations
-    {
+    {//Opértions de base
         public static void BasicOperation(int a, int b, char c)
         {
             int res123;
-                        
+     // Je fais un switch, en fonction de l'opérateur
             switch (c)
             {
                 case '+' :
                     res123 = a + b;
                     Console.WriteLine(a + " " + c + " " + b + " = " + res123);
                 break;
-
+     // Soustraction
                 case '-' :
                     res123 = a - b;
                     Console.WriteLine(a + " " + c + " " + b + " = " + res123);
                 break;
-
+     // division, avec un if pour le cas de la division par zero
                 case '/' :
                     if (b != 0)
                     {
@@ -37,13 +37,13 @@ namespace Serie_I
                      Console.WriteLine(a + " " + c + " " + b + " = " + "opération invalide"); 
 
                 break;
-
+     //Multiplication
                 case '*' :
                     res123 = a * b;
                     Console.WriteLine(a + " " + c + " " + b + " = " + res123);
 
                 break;
-
+     // Si un autre caractère a été utilisé comme opératur
                 default :
                 
                 Console.WriteLine(a + " " + c + " " + b + " = " + "opération invalide");
@@ -51,35 +51,46 @@ namespace Serie_I
             }
            
         }
-
+     //Division entière
         public static void IntegerDivision(int a, int b)
 
         {
             int q;
+            int r;
            
-
+     //If, pour gérer la division par 0       
             if (b == 0)
             { Console.WriteLine(a +" : " + b + " = Opération invalide" ); }
 
             else
-
+     
             {
                 q = a / b;
-                    
-                Console.WriteLine(a + " = " + b + "*" + q);
+                r = a % b;     
+                Console.WriteLine(a + " = "+ q + " * " + b + " + " + r);
 
             }
         }    
-
+     //Calcul d'une puissance
         public static void Pow(int a, int b)
         {
             int puiss;
+
+     //Cas d'une puissance négative
             if (b < 0)
             { Console.WriteLine(a + " ^ " + b + " = Opération invalide"); }
 
             else
-            { 
-                puiss = (a*a)*b;
+            {
+     //On va boucler en multipliant la variable a par elle même, b fois
+                puiss = 1;
+                int boucle = 0;
+                while (boucle < b)
+                {
+                    puiss = puiss * a;
+                    boucle++;
+                }
+
              Console.WriteLine(a + " ^ " + b + " = " + puiss); 
             }
         }
