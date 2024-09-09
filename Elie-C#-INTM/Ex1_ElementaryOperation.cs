c6cec2494721c657a3b51c8f7395d35b44c1681c
William <william.jaeger57@gmail.com>
2024-09-09 13:19:20 +0200

    avec exos 1

---
+++ Elie-C#-INTM/Ex1_ElementaryOperation.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_I
+{
+    public static class ElementaryOperations
+    {
+        public static void BasicOperation(int a, int b, char c)
+        {
+            int res123;
+                        
+            switch (c)
+            {
+                case '+' :
+                    res123 = a + b;
+                    Console.WriteLine(a + " " + c + " " + b + " = " + res123);
+                break;
+
+                case '-' :
+                    res123 = a - b;
+                    Console.WriteLine(a + " " + c + " " + b + " = " + res123);
+                break;
+
+                case '/' :
+                    if (b != 0)
+                    {
+                        res123 = a / b;
+                        Console.WriteLine(a + " " + c + " " + b + " = " + res123);
+                    }
+                    else
+                     Console.WriteLine(a + " " + c + " " + b + " = " + "opération invalide"); 
+
+                break;
+
+                case '*' :
+                    res123 = a * b;
+                    Console.WriteLine(a + " " + c + " " + b + " = " + res123);
+
+                break;
+
+                default :
+                
+                Console.WriteLine(a + " " + c + " " + b + " = " + "opération invalide");
+                break;
+            }
+           
+        }
+
+        public static void IntegerDivision(int a, int b)
+
+        {
+            int q;
+           
+
+            if (b == 0)
+            { Console.WriteLine(a +" : " + b + " = Opération invalide" ); }
+
+            else
+
+            {
+                q = a / b;
+                    
+                Console.WriteLine(a + " = " + b + "*" + q);
+
+            }
+        }    
+
+        public static void Pow(int a, int b)
+        {
+            int puiss;
+            if (b < 0)
+            { Console.WriteLine(a + " ^ " + b + " = Opération invalide"); }
+
+            else
+            { 
+                puiss = (a*a)*b;
+             Console.WriteLine(a + " ^ " + b + " = " + puiss); 
+            }
+        }
+    }
+    
+
+}
