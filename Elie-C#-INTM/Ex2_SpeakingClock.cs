408837064751b24b7509a1e1efba1fee98fc9662
William <william.jaeger57@gmail.com>
2024-09-09 15:52:04 +0200

    Exec 2 fini

--- Elie-C#-INTM/Ex2_SpeakingClock.cs
+++ Elie-C#-INTM/Ex2_SpeakingClock.cs
@@ -10,8 +10,54 @@ namespace Serie_I
     {
         public static string GoodDay(int heure)
         {
-            //TODO
-            return string.Empty;
+
+          //    En fonction de l'heure on va afficher un message différent      
+            switch (heure)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+
+                    Console.WriteLine("Il est " + heure + "H, merveilleuse nuit!");
+                break;
+
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:   
+
+                    Console.WriteLine("Il est " + heure + "H, Bonne matinée!");
+                break;
+
+                case 12:
+                    Console.WriteLine("Il est " + heure +  "H, Bon appétit!");
+                break;
+
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+
+                    Console.WriteLine("Il est " + heure + "H, Profitez de votre après midi!");
+                break;
+
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+
+                    Console.WriteLine("Il est " + heure + "H, passez une bonne soirée");
+                    break;
+            }
+                return string.Empty;
         }
     }
 }
