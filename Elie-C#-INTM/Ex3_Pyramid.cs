17fabc123b4bf1229432d1227fac472a1a3ad24c
William <william.jaeger57@gmail.com>
2024-09-10 10:21:37 +0200

    exo 4 fini

--- Elie-C#-INTM/Ex3_Pyramid.cs
+++ Elie-C#-INTM/Ex3_Pyramid.cs
@@ -19,14 +19,14 @@ namespace Serie_I
         /// <param name="isSmooth"> représente la texture de la pyramide </param>
         public static void PyramidConstruction(int n, bool isSmooth)
         {
-            
+         //On fait une 1ère boucle pour chaque étage de la pyramide (n étages)   
             for (int j = 1; j <= n; j++)
-
+         //On fait une 2nde boucle pour chaque blocs présents sur une ligne, dans la 1ère boucle.
             {
                 for (int t = 1; t <= n*2-1 ; t++)
-                {
+                {//Condition permettant de ne remplir que les blocs appartenant à la pyramide
                     if (t >= n - j + 1  && t <= n + j - 1 )
-                    {
+                    {//condition permettant de s'adapter à la texture de la pyramide
                         if (j % 2 == 0 && !isSmooth)
                         {
                             Console.Write("-");
--- Elie-C#-INTM/Ex4_Factorial.cs
+++ Elie-C#-INTM/Ex4_Factorial.cs
@@ -7,17 +7,43 @@ using System.Threading.Tasks;
 namespace Serie_I
 {
     public static class Factorial
-    {
+    {/// <summary>
+    /// 
+    /// </summary>
+    /// <param name="n"> n contient la valeur rentrée par l'utilisateur</param>
+    /// <returns></returns>
         public static int Factorial_(int n)
-        {
-            //TODO
-            return -1;
-        }
+        {//On ne peut faire la factorielle que pour les nombres positifs. On va donc faire un retour -1 en cas de négatif, et 1 quand n = 0.
+            int num1 = n;
+            if (n > 0)
 
+            {
+
+                for (int i = n - 1; i > 0; i--)
+                {
+                    num1 = num1 * i;
+                }
+            
+            return num1; 
+            }
+
+            if (n == 0)
+                 return 1;
+
+            else
+                return -1;
+        }
+        // On fait la même factorielle, mais de façon récursive.
         public static int FactorialRecursive(int n)
         {
-            //TODO
-            return -1;
+            
+            if (n == 0)
+            
+                return 1;  
+            
+            if (n < 0)
+                return -1;
+            return n * FactorialRecursive(n - 1);
         }
     }
 }
