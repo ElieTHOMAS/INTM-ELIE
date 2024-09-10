a43beae9f7d70ff90e31b2782f3f9d138d3190c0
William <william.jaeger57@gmail.com>
2024-09-10 13:20:50 +0200

    FIN série 1, exos 1 à 4 terminés, 5 pas complet

--- Elie-C#-INTM/Ex5_PrimeNumbers.cs
+++ Elie-C#-INTM/Ex5_PrimeNumbers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +11,52 @@ namespace Serie_I
     {
         static bool IsPrime(int valeur)
         {
-            //TODO
+
+            int racine2;
+
+            if (valeur > 0)
+            {
+               double racine  = Math.Sqrt(valeur);
+               racine2 = (int)racine;
+               
+
+                for (int i = racine2; i > 1; i--)
+                {
+
+                    if(valeur % i == 0)
+
+                                   
+                       return false;
+                }
+
+                return true;
+            }
+            else
             return false;
         }
 
         public static void DisplayPrimes()
         {
-            //TODO
+          
+            int r2;
+            int racine4;
+           
+
+             for (int i3 = 1; i3 < 101; i3 ++)
+                 {
+                    double racine3 = Math.Sqrt(i3);
+                    racine4 = (int)racine3;
+
+                    for (int i4 = racine4; i4 > 1; i4--)
+                    {
+                        r2 = i3 % i4;
+
+                        if (r2 != 0 && i4 > 1 )
+                        { Console.Write(i3 + " "); }
+
+                    }
+                  }
+        
         }
     }
 }
