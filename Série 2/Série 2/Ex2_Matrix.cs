8bf44b310d3f3c2ed0d5c109e16f02b5a583d95e
Elie <thomas4953@gmail.com>
2024-09-13 17:16:31 +0200

    exo 2

--- Série 2/Série 2/Ex2_Matrix.cs
+++ Série 2/Série 2/Ex2_Matrix.cs
@@ -45,9 +45,9 @@ namespace Serie_II
         public static void DisplayMatrix(int[,] matrix)
         {
             string s = string.Empty;
-            for (int i = 0; i < matrix.Length; ++i)
+            for (int i = 0; i < matrix.GetLength(0); ++i)
             {
-                for (int j = 0; j < matrix[i].Length; ++j)
+                for (int j = 0; j < matrix.GetLength(1); ++j)
                 {
                     s += matrix[i,j].ToString().PadLeft(5) + " ";
                 }
