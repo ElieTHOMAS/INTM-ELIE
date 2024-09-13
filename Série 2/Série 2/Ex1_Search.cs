6b7b86a30131296389ba056c78954fd442d3990f
Elie <thomas4953@gmail.com>
2024-09-13 14:21:20 +0200

    maj

--- Série 2/Série 2/Ex1_Search.cs
+++ Série 2/Série 2/Ex1_Search.cs
@@ -10,15 +10,33 @@ namespace Serie_II
     {
         public static int LinearSearch(int[] tableau, int valeur)
         {
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                if (tableau[i] == valeur)
+                {
+                    return i + 1;
+                }
+            }
 
-
-            //TODO
             return -1;
         }
 
         public static int BinarySearch(int[] tableau, int valeur)
         {
-            //TODO
+            int i = tableau.Length / 2;
+            while (tableau[i] != valeur)
+            { 
+                if (tableau[i] < valeur)
+                {
+                    i == i / 2;
+                }
+                if (tableau[i] > valeur)
+                {
+                    i == i * (i/2);
+                }
+                
+            }
+
             return -1;
         }
     }
