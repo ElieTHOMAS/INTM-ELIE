153b12fa58111deb35115ea92ace8b2933d08fd6
Elie <thomas4953@gmail.com>
2024-09-16 14:04:47 +0200

    MAJ

--- Série 3/Série 3/Ex1_ClassCouncil.cs
+++ Série 3/Série 3/Ex1_ClassCouncil.cs
@@ -9,9 +9,44 @@ namespace Serie_III
 {
     public static class ClassCouncil
     {
+        
         public static void SchoolMeans(string input, string output)
         {
-            //TODO
+          //  using (Stream s = new FileStream(" result.csv ", FileMode.Create)) ;
+          string line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                //StreamReader sr = new StreamReader(input);
+                using (FileStream inputStream = File.OpenRead(input)) 
+                {
+                    using (TextReader reader = new StreamReader(inputStream)) 
+                    {
+                        List<string> ent = new List <string>();
+                        //Read the first line of text
+                        line = reader.ReadLine();
+                        string[] linesort = line.Split(';');
+                        //Continue to read until you reach end of file
+                        while (line != null)
+                        {
+                            ent.Add(line);
+
+                        }
+                        //close the file
+                        Console.ReadLine();
+                    }
+                    
+                }
+            }
+               
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
         }
     }
 }
