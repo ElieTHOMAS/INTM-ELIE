c10ea53ab3d98183bb283b3ff1f2700aa023ac71
Elie <thomas4953@gmail.com>
2024-09-16 11:21:52 +0200

    exos 1 et 2(début) série II fait

--- Série 2/Série 2/Ex2_Matrix.cs
+++ Série 2/Série 2/Ex2_Matrix.cs
@@ -8,27 +8,56 @@ namespace Serie_II
 {
     public static class Matrix
     {
-        public static int[,] BuildingMatrix(int[] leftVector, int[] rightVector)
+        //public static int[,] BuildingMatrix(int[] leftVector, int[] rightVector)
+        //{
+        //    int[,] matrice1 = new int[leftVector.Length,rightVector.Length];
+
+        //    for (int i = 0; i < leftVector.Length; i++)
+        //    {
+        //        for (int j = 0; j < rightVector.Length; j++)
+        //        {
+        //            matrice1[i,j]=leftVector[i]* rightVector[j] ;    
+        //        }
+
+        //    }
+        //    return matrice1;
+        public static int[][] BuildingMatrix(int[] leftVector, int[] rightVector)
         {
-            int[,] matrice1 = new int[leftVector.Length,rightVector.Length];
-            
+            int[][] matrice1 = new int[leftVector.Length][];
+
             for (int i = 0; i < leftVector.Length; i++)
             {
+                matrice1[i] = new int[rightVector.Length];
+
                 for (int j = 0; j < rightVector.Length; j++)
                 {
-                    matrice1[i,j]=leftVector[i]* rightVector[j] ;    
+                    matrice1[i][j] = leftVector[i] * rightVector[j];
                 }
-             
+
             }
             return matrice1;
-
         }
 
-        //public static int[,] Addition(int[,] leftMatrix, int[,] rightMatrix)
-        //{
-        //    //TODO
-        //    return new int[,];
-        //}
+    public static int[][] Addition(int[][] leftMatrix, int[][] rightMatrix)
+        {
+            int[][] result = new int[leftMatrix.Length][];
+            if (leftMatrix.Length == rightMatrix.Length)
+            {
+                for (int i = 0; i < leftMatrix.Length; i++)
+                {
+                    if (leftMatrix[i].Length == rightMatrix[i].Length)
+                    {
+                        result[i] = new int[leftMatrix[i].Length];
+
+                        for (int j = 0;j < leftMatrix[i].Length; j++)
+                        {
+                            result[i][j] = leftMatrix[i][j] + rightMatrix[i][j];
+                        }
+                    }
+                }
+            }
+            return (result);
+        }
 
         //public static int[,] Substraction(int[,] leftMatrix, int[,] rightMatrix)
         //{
@@ -42,18 +71,17 @@ namespace Serie_II
         //    return new int[,];
         //}
 
-        public static void DisplayMatrix(int[,] matrix)
+        public static void DisplayMatrix(int[][] matrix)
         {
             string s = string.Empty;
-            for (int i = 0; i < matrix.GetLength(0); ++i)
+            for (int i = 0; i < matrix.Length; ++i)
             {
-                for (int j = 0; j < matrix.GetLength(1); ++j)
+                for (int j = 0; j < matrix[i].Length; ++j)
                 {
-                    s += matrix[i,j].ToString().PadLeft(5) + " ";
+                    Console.WriteLine(matrix[i][j] + " ");
                 }
-                s += Environment.NewLine;
+                Console.WriteLine("");
             }
-            Console.WriteLine(s);
         }
     }
 }
