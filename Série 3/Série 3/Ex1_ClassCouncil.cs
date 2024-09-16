49a23732a509f6bd852ec798d22ec7debdc725b3
ElieTHOMAS <thomas4953@gmail.com>
2024-09-16 14:56:15 +0200

    Update Ex1_ClassCouncil.cs

--- Série 3/Série 3/Ex1_ClassCouncil.cs
+++ Série 3/Série 3/Ex1_ClassCouncil.cs
@@ -9,7 +9,44 @@ namespace Serie_III
 {
     public static class ClassCouncil
     {
-        
+        static void Main(string[] args)
+        {
+            SchoolMeans("Input.csv", "Output.csv");
+        }
+
+        static string[] Moyennes (List<string> entrees)
+        {
+            double HTotal = 0;
+            double MTotal = 0;
+            int nombreH = 0;
+            int nombreM = 0;
+            if (matiere == "Histoire")
+                {
+                    HTotal += double.Parse(note);
+                    nHist += 1;
+                }
+                else if (matiere == "Maths")
+                {
+                    MTotal += double.Parse(note);
+                    nMath += 1;
+                }
+            }
+
+            double histMoyenne = HTotal / nombreH;
+            double mathMoyenne = MTotal / nombreM;
+            string[] sorties = new string[2];
+            sorties[0] = "Histoire;" + histMoyenne.ToString();
+            sorties[1] = "Maths;" + mathMoyenne.ToString();
+
+            return (sorties);
+        }
+            foreach (string ligne in entrees)
+            {
+                string matiere = ligne.Split(';')[1];
+                string note = ligne.Split(';')[2];
+                note = note.Replace('.', ',');
+
+
         public static void SchoolMeans(string input, string output)
         {
           //  using (Stream s = new FileStream(" result.csv ", FileMode.Create)) ;
@@ -27,13 +64,23 @@ namespace Serie_III
                         line = reader.ReadLine();
                         string[] linesort = line.Split(';');
                         //Continue to read until you reach end of file
-                        while (line != null)
+                        while (reader != null)
                         {
                             ent.Add(line);
-
                         }
-                        //close the file
-                        Console.ReadLine();
+                        string [] sorties = Average(ent);
+                        using (FileStream outputStream = File.OpenWrite(output))
+                        {
+                          using (TextWriter redac = new StreamWriter(outputStream))
+                          {
+                            foreach (string ligne in sorties)
+                            {
+                                redac.WriteLine(ligne);
+                            }
+                          }
+                        }
+    
+                       
                     }
                     
                 }
