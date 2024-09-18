4e28fe52e6eec5e51225c8ecf725743f6d1a9ac9
Elie <thomas4953@gmail.com>
2024-09-18 11:48:39 +0200

    maj

---
+++ Projet C# Elie THOMAS/Projet C# Elie THOMAS/comptes input/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace comptes_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
