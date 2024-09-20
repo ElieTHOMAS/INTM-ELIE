e87e2251f03f7341b5557a05a0a057a5c4eb5b26
Elie <thomas4953@gmail.com>
2024-09-20 11:09:16 +0200

    début partie II

--- Projet C# Elie THOMAS-partie 2/Projet C# Elie THOMAS/projetprogram/Fichier_Compte.cs
+++ Projet C# Elie THOMAS-partie 2/Projet C# Elie THOMAS/projetprogram/Fichier_Compte.cs
@@ -10,14 +10,18 @@ namespace projetprogram
 {
     internal class Comptes
     {
-        static string comptes = @"C:\\Users\\Formation\\source\\repos\\INTM-ELIE\\compte.csv";
+        static string comptes = @"C:\\Users\\Formation\\source\\repos\\INTM-ELIE\\\Projet C# Elie THOMAS-partie 2\\compte.csv";
         static List<Comptes> fichierC;
         internal int clients { get; set; }
+        internal string date { get; set; }
         internal decimal solde { get; set; }
+        internal int entree { get; set; }
+        internal int sortie { get; set; }
         internal int compteur { get; set; }
         internal decimal cumul { get; set; }
+      
 
-
+        C:\Users\Formation\source\repos\INTM-ELIE\Projet C# Elie THOMAS-partie 2
         internal static List<Comptes> input2()
         {
             string line;
@@ -39,11 +43,17 @@ namespace projetprogram
                             Comptes accounts = new Comptes();
                             int b;
                             int c;
+                            string date;
+                            int ent;
+                            int sor;
                             int.TryParse(linesort[0], out b);
                             int.TryParse(linesort[1], out c);
 
                             accounts.clients = b;
                             accounts.solde = c;
+                            accounts.date = date;
+                            accounts.entree = ent;
+                            accounts.sortie = sor;
                             fichierC.Add(accounts);
                             line = reader.ReadLine();
                         }
